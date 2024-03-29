e7ecb9f4c8340879d131a3cf5402c6a582a0e4b4
oddegen <nahomtensae@gmail.com>
2024-03-29 17:06:55 +0300

    render torus

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,24 @@
-import { useEffect, useState } from "react";
 import { Canvas, ThreeElements } from "@react-three/fiber";
-import { Physics, usePlane, useBox } from "@react-three/cannon";
-import { BufferGeometry, Mesh } from "three";
-
-function Plane(props: ThreeElements["planeBufferGeometry"]) {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
-  return (
-    <mesh receiveShadow ref={ref as React.RefObject<Mesh<BufferGeometry>>}>
-      <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial color="#f0f0f0" />
-    </mesh>
-  );
-}
-
-function Cube(props: ThreeElements["boxBufferGeometry"]) {
-  const [ref] = useBox(() => ({ mass: 1, ...props }));
-  return (
-    <mesh castShadow ref={ref as React.RefObject<Mesh<BufferGeometry>>}>
-      <boxGeometry />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
-}
+import { OrbitControls, Environment } from "@react-three/drei";
 
 export default function App() {
-  const [ready, set] = useState(false);
-  useEffect(() => {
-    const timeout = setTimeout(() => set(true), 1000);
-    return () => clearTimeout(timeout);
-  }, []);
   return (
-    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Canvas dpr={[1, 2]} shadows camera={{ position: [-5, 5, 5], fov: 50 }}>
-        <ambientLight />
-        <spotLight angle={0.25} penumbra={0.5} position={[10, 10, 5]} castShadow />
-        <Physics>
-          <Plane />
-          <Cube position={[0, 5, 0]} />
-          <Cube position={[0.45, 7, -0.25]} />
-          <Cube position={[-0.45, 9, 0.25]} />
-          {ready && <Cube position={[-0.45, 10, 0.25]} />}
-        </Physics>
+    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full">
+      <Canvas>
+        <ambientLight intensity={0.5 * Math.PI} />
+        <Torus scale={1.75} />
+        <OrbitControls />
+        <Environment preset="city" />
       </Canvas>
     </div>
+  );
+}
+
+function Torus(props: ThreeElements["mesh"]) {
+  return (
+    <mesh {...props}>
+      <torusGeometry args={[1, 0.25, 32, 100]} />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
   );
 }
